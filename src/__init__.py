af3f7ce45dfd58afa45cc5998341b872e37888e0
Laura Tesoriero <Laurartesoriero@gmail.com>
2019-04-24 18:26:10 -0400

    architecture

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,19 @@
-print("~~~Budget App Mobile~~~ \n\t-- Main Menu --")
-user_choice = int(input("""
-1. Existing User
-2. New User
-3. Exit Application
+# This Function Stars up the temp non-complicated GUI
+def __init__():
+    print("~~~Budget App Mobile~~~ \n\t-- Main Menu --")
+    user_choice = int(input("""
+    1. Existing User
+    2. New User
+    3. Exit Application
+    
+    Enter Corresponding Number for Choice: """))
 
-Enter Corresponding Number for Choice:
-"""))
+    if user_choice == 1:
+        print(1)
+    elif user_choice == 2:
+        print(2)
+    else:
+        exit()
 
-if user_choice == 1:
-    print(1)
-elif user_choice == 2:
-    print(2)
-else:
-    exit()
+
+__init__()
