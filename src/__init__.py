4da93270899b2906f4bf53f92012f56670686440
Laura Tesoriero <Laurartesoriero@gmail.com>
2019-04-24 14:03:01 -0400

    architecture

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+print("~~~Budget App Mobile~~~ \n\t-- Main Menu --")
+user_choice = int(input("""
+1. Existing User
+2. New User
+3. Exit Application
+
+Enter Corresponding Number for Choice:
+"""))
+
+if user_choice == 1:
+    print(1)
+elif user_choice == 2:
+    print(2)
+else:
+    exit()
