ee570e4a978adc868ad6d71fe0ab33ba3de0bdbd
Laura Tesoriero <Laurartesoriero@gmail.com>
2019-04-25 16:43:49 -0400

    changed log in structure for existing users

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,22 @@
-import money_management.py
+from money_management import *
 
-# This Function Stars up the temp non-complicated GUI
-
-def __init__():
+# This Function Stars up the GUI for person to interact with
+def main():
     print("~~~Budget App Mobile~~~ \n\t-- Main Menu --")
     user_choice = int(input("""
     1. Existing User
     2. New User
     3. Exit Application
-    
     Enter Corresponding Number for Choice: """))
 
     if user_choice == 1:
-        print(1)
+        userinput = input("Welcome! Please Enter your Username: \n")
+        userpassword = input("Please enter your 4 digit pin code: \n")
+        login(userinput, userpassword)
     elif user_choice == 2:
-        Bank()
+        print(2)
     else:
         exit()
 
 
-__init__()
+main()
--- src/money_management.py
+++ src/money_management.py
@@ -1,47 +1,55 @@
+# this function allows existing users to log in
+def login(userinput, userpassword):
 
-class LogIn(object):
-    def user_name(self, username, password):
-        self.username = username
-        self.password = password
-
-    def checkusername(self, input_username):
-        if input_username == self.username:
-            input_password = input("Password: \n")
-            if input_password == self.password:
-                print("Welcome " + self.username + " !")
+    def password_check(userpassword):
+        attempts = 3
+        while attempts > 0:
+            if userpassword in passwords:
+                return True
             else:
-                print("Incorrect Password")
+                attempts -= 1
+                userpassword = input("Incorrect Code." + str(attempts) + " attempts remaining \nplease try again: \n")
+
+        print("User Account is not available at this time please try again later, Thank you.")
+        exit()
+    usernames, passwords, balances = user_files()
+    if userinput in usernames:
+        password_check(userpassword)
+        print("User Menu")
+    else:
+        choice = input("""User not found
+        Press 1 to Restart
+        Press 2 to Create New User
+        Press 3 to Exit
+        """)
+        if choice == 1:
+            userinput = input("Welcome! Please Enter your Username: \n")
+            userpassword = input("Please enter your 4 digit pin code: \n")
+            login(userinput, userpassword)
+        elif choice == 2:
+            print("make new user function")
         else:
-            print("User not found \n ")
+            exit()
+
+# This function reads all stored user information and parses the files to store into lists
+def user_files():
+    usernames = []
+    passwords = []
+    balances = []
+
+    with open("User_Names.txt", 'r') as usernamefile:
+        for line in usernamefile:
+            usernames.append(line[:-1])
+    with open("Passwords.txt", 'r') as passfile:
+        for line in passfile:
+            passwords.append(line[:-1])
+    with open("Balance.txt", 'r') as balancefile:
+        for line in balancefile:
+            balances.append(line[:-1])
+
+    return usernames, passwords, balances
+
+#class BankingActions(object):
+#    def user_functions(self):
 
 
-class ClientProfile(LogIn):
-    def __init__(self, total_income=0):
-        self.totalincome = total_income
-
-
-class Transaction(ClientProfile):
-    def user_income(self):
-        input_income = int(input('Total Income Received: '))
-        self.totalincome += input_income
-        return self.totalincome
-
-    def user_expense(self):
-        input_expense = int(input('Expense Amount: '))
-        self.totalincome -= input_expense
-
-
-# controls the accounts and storage of information
-class Bank(object):
-    def __init__(self):
-        self.username
-        self.password
-        self.balance = filestore.customeraccount()
-
-
-# manages balance of an account
-class BankAccount(object):
-
-
-# in charge of input, output, printing, and calling
-class Interactive(object):
