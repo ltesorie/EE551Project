5e430cc536dc87b868912e81cf3ddda369b47c4a
Laura Tesoriero <Laurartesoriero@gmail.com>
2019-04-26 14:56:06 -0400

    starting unittest file

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -1,15 +1,19 @@
 import unittest
-from unittest.mock import patch
-import money_management
-import __init__
+\money_management
+\__init__
 
 class TestLogInUser(unittest.TestCase):
 
     def test_checkpassword(self):
-        self.
+        checkpassword = checkpassword()
+        self.assertFalse(checkpassword)
+
 
 # class TestStartUp(unittest.TestCase):
 
 # class TestLogInNewUser(unittest.TestCase):
 
 # class TestBankActions(unittest.TestCase):
+
+if __name__ == '__main__':
+    unittest.main()
